06a572cb304f425c5fa70b167f22eda25705f80e
Alex Brady <alexbrady1111@gmail.com>
2025-07-09 14:26:31 +0100

    Adding the grid and also adding query selectors for each HTML element

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
 import "./style.css";
+
+// Firstly I should set up a grid system that will allow tracking off the snake and the food item
+// Define the initial state of each item and the direction
+type Position = { x: number; y: number };
+
+let snake: Position[] = [{ x: 10, y: 10 }];
+let food: Position = { x: 5, y: 5 };
+let direction: Position = { x: 0, y: 0 };
+let score: number = 0;
+
+// Grabbing each element with a query selector
+const stageElement = document.querySelector<HTMLDivElement>(
+    ".main--container__stage"
+) as HTMLDivElement;
+const snakeElement = document.querySelector<HTMLDivElement>(
+    ".main--container__stage-snake"
+) as HTMLDivElement;
+const foodElement = document.querySelector<HTMLDivElement>(
+    ".main--container__stage-food"
+) as HTMLDivElement;
+const scoreElement = document.querySelector<HTMLDivElement>(
+    ".main-container__score-label span"
+) as HTMLSpanElement;
+
+
+// I will need to create an event listener that handles the wasd keys and then map then to the buttons in put as well
+// Alternatively I could just use the same direction event listener for the button i.e. -y -1 x 0 etc 
+
+
+
+
